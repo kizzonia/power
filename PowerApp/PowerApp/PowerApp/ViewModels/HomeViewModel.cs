4cf4997879cd7fdfcd80b2a715619209cc093f4a
fortune chibuike <chibuikefortune@hotmail.com>
2017-05-23 13:26:49 +0100

    ViewModels And First Navigaton Page Done

--- PowerApp/PowerApp/PowerApp/ViewModels/HomeViewModel.cs
+++ PowerApp/PowerApp/PowerApp/ViewModels/HomeViewModel.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using PowerApp.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace PowerApp.ViewModels
 {
-    class HomeViewModel
+    class HomeViewModel : BaseViewModel
     {
+        private INavigation Navigation;
+        public HomeViewModel(INavigation _Navigation)
+        {
+            HydroACommand = new Command(HydroAPage);
+            HydroBCommand = new Command(HydroBPage);
+
+            Navigation = _Navigation;
+        }
+
+        public ICommand HydroACommand { private set; get; }
+        public ICommand HydroBCommand { private set; get; }
+
+        public void HydroAPage()
+        {
+            Navigation.PushAsync(new HydroA());
+        }
+
+        public void HydroBPage()
+        {
+            Navigation.PushAsync(new HydroB());
+        }
+
     }
 }
