6c3272f1e8bae45c43bd8cea4f2aef7f72736cb5
fortune chibuike <chibuikefortune@hotmail.com>
2017-05-23 13:18:25 +0100

    splash and sql and json

---
+++ PowerApp/PowerApp/PowerApp.Android/SplashActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PowerApp.Droid
+{
+
+    [Activity(Theme = "@style/Theme.Splash",
+         MainLauncher = true,
+          NoHistory = true,
+          Label = "PowerApp")]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
